f58ed5524ed9afb2185759c7941d7f79a7b41791
Dennnnis <dennisbeckertim@gmail.com>
2018-09-03 11:36:54 +0200

    Initial commit

---
+++ Game.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace TorenVanHanoi
+{
+    partial class Game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.disk8 = new System.Windows.Forms.Button();
+            this.disk7 = new System.Windows.Forms.Button();
+            this.disk6 = new System.Windows.Forms.Button();
+            this.disk5 = new System.Windows.Forms.Button();
+            this.disk4 = new System.Windows.Forms.Button();
+            this.disk3 = new System.Windows.Forms.Button();
+            this.disk2 = new System.Windows.Forms.Button();
+            this.disk1 = new System.Windows.Forms.Button();
+            this.Updater = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.panel1.Location = new System.Drawing.Point(304, 150);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(16, 500);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.panel2.Location = new System.Drawing.Point(608, 150);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(16, 500);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.panel3.Location = new System.Drawing.Point(896, 150);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(16, 500);
+            this.panel3.TabIndex = 2;
+            // 
+            // disk8
+            // 
+            this.disk8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(140)))), ((int)(((byte)(0)))));
+            this.disk8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(140)))), ((int)(((byte)(0)))));
+            this.disk8.Location = new System.Drawing.Point(326, 433);
+            this.disk8.Name = "disk8";
+            this.disk8.Size = new System.Drawing.Size(240, 30);
+            this.disk8.TabIndex = 3;
+            this.disk8.Text = "8";
+            this.disk8.UseVisualStyleBackColor = false;
+            // 
+            // disk7
+            // 
+            this.disk7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(140)))), ((int)(((byte)(0)))));
+            this.disk7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(140)))), ((int)(((byte)(0)))));
+            this.disk7.Location = new System.Drawing.Point(326, 397);
+            this.disk7.Name = "disk7";
+            this.disk7.Size = new System.Drawing.Size(220, 30);
+            this.disk7.TabIndex = 4;
+            this.disk7.Text = "7";
+            this.disk7.UseVisualStyleBackColor = false;
+            // 
+            // disk6
+            // 
+            this.disk6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(140)))), ((int)(((byte)(0)))));
+            this.disk6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(140)))), ((int)(((byte)(0)))));
+            this.disk6.Location = new System.Drawing.Point(326, 361);
+            this.disk6.Name = "disk6";
+            this.disk6.Size = new System.Drawing.Size(200, 30);
+            this.disk6.TabIndex = 6;
+            this.disk6.Text = "6";
+            this.disk6.UseVisualStyleBackColor = false;
+            // 
+            // disk5
+            // 
+            this.disk5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(140)))), ((int)(((byte)(0)))));
+            this.disk5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(140)))), ((int)(((byte)(0)))));
+            this.disk5.Location = new System.Drawing.Point(326, 325);
+            this.disk5.Name = "disk5";
+            this.disk5.Size = new System.Drawing.Size(180, 30);
+            this.disk5.TabIndex = 5;
+            this.disk5.Text = "5";
+            this.disk5.UseVisualStyleBackColor = false;
+            // 
+            // disk4
+            // 
+            this.disk4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(140)))), ((int)(((byte)(0)))));
+            this.disk4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(140)))), ((int)(((byte)(0)))));
+            this.disk4.Location = new System.Drawing.Point(326, 289);
+            this.disk4.Name = "disk4";
+            this.disk4.Size = new System.Drawing.Size(160, 30);
+            this.disk4.TabIndex = 8;
+            this.disk4.Text = "4";
+            this.disk4.UseVisualStyleBackColor = false;
+            // 
+            // disk3
+            // 
+            this.disk3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(140)))), ((int)(((byte)(0)))));
+            this.disk3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(140)))), ((int)(((byte)(0)))));
+            this.disk3.Location = new System.Drawing.Point(326, 253);
+            this.disk3.Name = "disk3";
+            this.disk3.Size = new System.Drawing.Size(140, 30);
+            this.disk3.TabIndex = 7;
+            this.disk3.Text = "3";
+            this.disk3.UseVisualStyleBackColor = false;
+            // 
+            // disk2
+            // 
+            this.disk2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(140)))), ((int)(((byte)(0)))));
+            this.disk2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(140)))), ((int)(((byte)(0)))));
+            this.disk2.Location = new System.Drawing.Point(326, 217);
+            this.disk2.Name = "disk2";
+            this.disk2.Size = new System.Drawing.Size(120, 30);
+            this.disk2.TabIndex = 10;
+            this.disk2.Text = "2";
+            this.disk2.UseVisualStyleBackColor = false;
+            // 
+            // disk1
+            // 
+            this.disk1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(140)))), ((int)(((byte)(0)))));
+            this.disk1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(140)))), ((int)(((byte)(0)))));
+            this.disk1.Location = new System.Drawing.Point(326, 181);
+            this.disk1.Name = "disk1";
+            this.disk1.Size = new System.Drawing.Size(100, 30);
+            this.disk1.TabIndex = 9;
+            this.disk1.Text = "1";
+            this.disk1.UseVisualStyleBackColor = false;
+            // 
+            // Updater
+            // 
+            this.Updater.Interval = 1;
+            this.Updater.Tick += new System.EventHandler(this.Updater_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Deze game is saai";
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(140)))), ((int)(((byte)(0)))));
+            this.BackgroundImage = global::TorenVanHanoi.Properties.Resources.room;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1184, 561);
+            this.Controls.Add(this.disk2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.disk1);
+            this.Controls.Add(this.disk3);
+            this.Controls.Add(this.disk4);
+            this.Controls.Add(this.disk6);
+            this.Controls.Add(this.disk5);
+            this.Controls.Add(this.disk7);
+            this.Controls.Add(this.disk8);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Game";
+            this.ShowIcon = false;
+            this.Text = "Toren van hanoi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button disk8;
+        private System.Windows.Forms.Button disk7;
+        private System.Windows.Forms.Button disk6;
+        private System.Windows.Forms.Button disk5;
+        private System.Windows.Forms.Button disk4;
+        private System.Windows.Forms.Button disk3;
+        private System.Windows.Forms.Button disk2;
+        private System.Windows.Forms.Button disk1;
+        private System.Windows.Forms.Timer Updater;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Game.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TorenVanHanoi
+{
+    public partial class Game : Form
+    {
+        Button[] Buttons = new Button[8];
+        bool[] holding = { false, false, false, false, false, false, false, false };
+
+        Stack<int>[] towers = new Stack<int>[3];
+        Panel[] sticks = new Panel[3];
+
+        public Stack<int> Locate(int num)
+        {
+            for (int i = 0; i < towers.Length; i++)
+            {
+                if (towers[i].Contains(num))
+                {
+                    return towers[i];
+                }
+            }
+            throw new Exception();
+        }
+
+        public void Clip()
+        {
+            for (int t = 0; t < towers.Length; t++)
+            {
+                int[] unboxed = new int[towers[t].Count];
+                (towers[t]).CopyTo(unboxed, 0);
+
+                int Y = 560 - (towers[t].Count * 30);
+                int X = sticks[t].Location.X + sticks[t].Size.Width / 2;
+                for (int i = 0; i < towers[t].Count; i++)
+                {
+                    var target = Buttons[unboxed[i]];
+                    target.Location = new Point(X - target.Size.Width/2,Y + i * 30);
+                }
+            }
+        }
+
+        public bool Colide(Button a, Panel b)
+        {
+            int a_r = a.Location.X + a.Size.Width;
+            int a_l = a.Location.X;
+
+            int b_r = b.Location.X + b.Size.Width;
+            int b_l = b.Location.X;
+
+            if (a_r > b_l && a_l < b_r)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public Game()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 3; i++) { towers[i] = new Stack<int>(); }
+
+            sticks[0] = panel1;
+            sticks[1] = panel2;
+            sticks[2] = panel3;
+
+            Buttons[0] = disk1; towers[0].Push(7);
+            Buttons[1] = disk2; towers[0].Push(6);
+            Buttons[2] = disk3; towers[0].Push(5);
+            Buttons[3] = disk4; towers[0].Push(4);
+            Buttons[4] = disk5; towers[0].Push(3);
+            Buttons[5] = disk6; towers[0].Push(2);
+            Buttons[6] = disk7; towers[0].Push(1);
+            Buttons[7] = disk8; towers[0].Push(0);
+
+            foreach (Button b in Buttons) b.MouseDown += Grabbed;
+            foreach (Button b in Buttons) b.MouseUp += Released;
+
+            Clip();
+        }
+
+        public void Released(object o, EventArgs args)
+        {
+            Button but = o as Button;
+            int size = Convert.ToInt32(but.Text) - 1;
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (Colide(but,sticks[i]))
+                {
+                    var from = Locate(size);
+                    var to = towers[i];
+
+                    if (to.Count > 0)
+                    {
+                        //Dont place it if
+                        if (to.Peek() < size)
+                        {
+                            break;
+                        }
+                    }
+                    to.Push(from.Pop());
+                }
+            }
+            holding[size] = false;
+            Updater.Stop();
+            Clip();
+        }
+
+        public void Grabbed(object o, EventArgs args)
+        {
+            Button Button = o as Button;
+            int size = Convert.ToInt32(Button.Text) - 1;
+
+            var f = Locate(size);
+            if (size == f.Min())
+            { 
+                holding[size] = true;
+                Updater.Start();
+            }
+        }
+
+        private void Updater_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < holding.Length; i++)
+            {
+                if (holding[i])
+                {
+                    Buttons[i].Location = new Point(this.PointToClient(Cursor.Position).X-Buttons[i].Size.Width/2, this.PointToClient(Cursor.Position).Y-Buttons[i].Height/2);
+                }
+            }
+        }
+    }
+}
